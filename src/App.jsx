75809ee92d99d01fb2708dd8f7c35ed0d81a7413
Auri Gabriel <ririmello.a@gmail.com>
2025-08-11 09:35:37 -0300

    feat: hero improvements

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,10 @@ const translations = {
     },
     aboutUsText: 'This project is part of the GEInfoEdu Research Group',
     students: 'Students involved in the project:',
-    // ...add more as needed...
+    hero: {
+      title: 'The Framework',
+      text: "In the development of software for the educational domain, a number of aspects must be taken into account, related to human information functions and the Information Technology (IT) Platform. Based on Ronald Stamper's Semiotic Framework, this framework adopts the Participatory Design approach, integrating Software Engineering in the development of software solutions for the Educational Domain.",
+    },
   },
   pt_BR: {
     loading: 'Carregando...',
@@ -63,7 +66,10 @@ const translations = {
     },
     aboutUsText: 'Este projeto é parte do Grupo de Pesquisa GEInfoEdu',
     students: 'Estudantes envolvidos no projeto:',
-    // ...add more as needed...
+    hero: {
+      title: 'O Framework',
+      text: 'No desenvolvimento de software para o domínio educacional, uma série de aspectos deve ser levada em conta, relacionadas às funções humanas da informação e à Plataforma de Tecnologia da Informação (TI). Baseado no Framework Semiótico de Ronald Stamper, este framework adota a abordagem do Design Participativo integrando a Engenharia de Software no desenvolvimento de soluções em software para o Domínio Educacional.',
+    },
   },
 };
 
