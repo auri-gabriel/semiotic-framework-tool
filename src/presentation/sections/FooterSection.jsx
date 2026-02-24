8243d20f62183c5006b0fa9f93f7edaf29c3baf0
Auri Gabriel <ririmello.a@gmail.com>
2026-02-23 21:08:50 -0300

    feat: enhance footer section with licensing information and GitHub link

--- src/presentation/sections/FooterSection.jsx
+++ src/presentation/sections/FooterSection.jsx
@@ -3,10 +3,14 @@ import { useLanguage } from '../hooks/useLanguage';
 
 const texts = {
   en: {
-    copyright: '© 2025 GEInfoEdu. All Rights Reserved.',
+    copyright: '© 2025 GEInfoEdu.',
+    licensedUnder: 'Licensed under',
+    github: 'Source code on GitHub',
   },
   pt_BR: {
-    copyright: '© 2025 GEInfoEdu. Todos os direitos reservados.',
+    copyright: '© 2025 GEInfoEdu.',
+    licensedUnder: 'Licenciado sob',
+    github: 'Código-fonte no GitHub',
   },
 };
 
@@ -16,7 +20,28 @@ const FooterSection = () => {
   return (
     <footer className='bg-dark text-white text-center py-5'>
       <div className='container'>
-        <small>{t.copyright}</small>
+        <small className='d-flex flex-column gap-1'>
+          <span>{t.copyright}</span>
+          <span>
+            {t.licensedUnder}{' '}
+            <a
+              href='https://www.gnu.org/licenses/gpl-3.0.html'
+              target='_blank'
+              rel='noreferrer'
+              className='link-light'
+            >
+              GNU GPL v3
+            </a>
+          </span>
+          <a
+            href='https://github.com/auri-gabriel/semiotic-framework-tool'
+            target='_blank'
+            rel='noreferrer'
+            className='link-light'
+          >
+            {t.github}
+          </a>
+        </small>
       </div>
     </footer>
   );
